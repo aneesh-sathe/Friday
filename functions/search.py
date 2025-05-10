b6f9b992e7148b3542122c84a1d29d294d66c64d
aneesh-sathe <aneesh.sathe07@gmail.com>
2025-05-10 23:23:30 +0530

    :add generate_links()

--- functions/search.py
+++ functions/search.py
@@ -1,41 +1,58 @@
+import ast
 import re
+from time import sleep
 
+import ollama
 from duckduckgo_search import DDGS
-from ollama import ChatResponse, chat
 
-keywords = ["Tata Motors"]
-
-
-def generate_query(model: str, keywords: list[str]):
-    response: ChatResponse = chat(
-        model=model,
-        messages=[
-            {
-                "role": "user",
-                "content": f"""You are a stock research assistant that generates concise, high-quality search queries for a downstream search function.
-
-                Your primary objective is to prioritize the most relevant and timely information — including current news, financial reports, earnings announcements, regulatory changes, mergers and acquisitions, and other recent developments — related to the following keywords: {keywords}.
-
-                Return 5 to 10 specific and well-structured search queries that would yield the most up-to-date results.
-
-                Return type: a Python list of strings. Return ONLY the list. No explanation, no preamble, no formatting.""",
-            },
-        ],
-    )
-    content = response["message"]["content"]
-
-    if "<think>" in content and "</think>" in content:
-        content = re.sub(r"<think.*?</think>", "", content, flags=re.DOTALL)
-
-    print(content)
+keywords = ["Tata Motors", "2025", "2024"]
 
 
-def search_web(query: str, max_results: int):
-    res = DDGS().text(query, max_results=max_results)
-    links = [body["href"] for body in res]
-    return links
+def generate_query(model: str, keywords: list[str]) -> list[str] | None:
+    try:
+        response: ollama.ChatResponse = ollama.chat(
+            model=model,
+            messages=[
+                {
+                    "role": "user",
+                    "content": f"""You are a stock research assistant that generates concise, high-quality search queries for a downstream search function.
+
+                    Your primary objective is to prioritize the most relevant and timely information — including current news, financial reports, earnings announcements, regulatory changes, mergers and acquisitions, and other recent developments — related to the following keywords: {keywords}.
+
+                    Return 5 to 10 specific and well-structured search queries that would yield the most up-to-date results.
+
+                    Return type: a Python list of strings. Return ONLY the list. No explanation, no preamble, no formatting.""",
+                },
+            ],
+        )
+        content = response["message"]["content"]
+
+        if "<think>" in content and "</think>" in content:
+            content = re.sub(r"<think.*?</think>", "", content, flags=re.DOTALL)
+        query_list = ast.literal_eval(content)
+        return query_list
+    except ollama.ResponseError as e:
+        print("Error:", e.error)
+        if e.status_code == 404:
+            ollama.pull(model)
+
+
+def generate_links(keywords: list[str], model: str, max_results: int):
+    links = []
+    search_list = generate_query(model=model, keywords=keywords)
+    # print(search_list)
+    if search_list:
+        for query in search_list:
+            search_result = DDGS().text(query, max_results=max_results)
+            sleep(3)
+            print(search_result)
+            for body in search_result:
+                links.append(body["href"])
+        return links
+    else:
+        print("Unable to generate links")
 
 
 if __name__ == "__main__":
     # print(search_web("zomato annual report 2024 site:https://www.screener.in/", 5))
-    generate_query("qwen3:4b", keywords)
+    print(generate_links(keywords, "qwen3:4b", 2))
