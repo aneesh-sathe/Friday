195ad127a9f046b034c283c435937a1cc6e6ad78
aneesh-sathe <aneesh.sathe07@gmail.com>
2025-05-12 10:41:58 +0530

    add: ollama embeddings

--- functions/embeddings.py
+++ functions/embeddings.py
@@ -1,3 +1,11 @@
 import chromadb
+import ollama
+from chromadb import Documents, EmbeddingFunction, Embeddings
 
 chroma_client = chromadb.Client()
+
+
+class OllamaEmbeddings(EmbeddingFunction):
+    def __call__(self, input: Documents) -> Embeddings:
+        response = ollama.embeddings(model="nomic-embed-text", prompt=input)
+        return response["embedding"]
