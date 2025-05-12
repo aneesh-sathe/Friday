bff3441659320dc9c4103b8d8b633be7324f12bf
aneesh-sathe <aneesh.sathe07@gmail.com>
2025-05-12 11:16:41 +0530

    add: addToVectorDB()

--- functions/embeddings.py
+++ functions/embeddings.py
@@ -1,12 +1,15 @@
 import os
 
 import chromadb
+from chromadb.config import Settings
 from chromadb.utils.embedding_functions import OllamaEmbeddingFunction
 from crawler import CrawledResult
 
 
 def getVectorDB() -> tuple[chromadb.Client, chromadb.Collection]:
-    client = chromadb.PersistentClient(path=f"{os.getcwd()}")
+    client = chromadb.PersistentClient(
+        path=f"{os.getcwd()}", settings=Settings(anonymized_telemetry=False)
+    )
 
     ollama_ef = OllamaEmbeddingFunction(
         url="http://localhost:11434/api/embeddings",
@@ -14,15 +17,29 @@ def getVectorDB() -> tuple[chromadb.Client, chromadb.Collection]:
     )
 
     collection = client.get_or_create_collection(
-        name="search_embeddings",
-        embedding_function=ollama_ef,
-        metadata={"hsnw:space": "cosine"},
+        name="llm_websearch",
+        configuration={
+            "hnsw": {
+                "space": "cosine",
+                "ef_search": 100,
+                "ef_construction": 100,
+                "max_neighbors": 16,
+                "num_threads": 4,
+            },
+            "embedding_function": ollama_ef,
+        },
     )
 
     return (client, collection)
 
 
 def addToVectorDB(crawled_result: list[CrawledResult]):
+    _, collection = getVectorDB()
+    for idx, res in enumerate(crawled_result):
+        collection.add(documents=[res.text], metadatas=[{"source": res.url}], ids=[idx])
+
+
+def get_top_k(query: str):
     pass
 
 
